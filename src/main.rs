0a67f39fa785460fb30db33f531351e500d4423c
Ahmet Enes Turan <ahmetenesturan@gmail.com>
2023-07-02 14:57:46 +0300

    better outputs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use clap::{Parser, builder::Str};
+use clap::Parser;
+
+//parser struct
 #[derive(Parser)]
 struct CliArguments{
     ///Name of the first currency
@@ -13,14 +15,16 @@ struct CliArguments{
 }
 
 fn match_currency_name(in_cur:&String) -> String{
-    let mut out_cur:String;
+    let out_cur:String;
     match in_cur.as_str(){
         "us_dollar" => out_cur = String::from("US Dollar"),
-        _ => out_cur = String::from("US Dollar"),
+        "euro" => out_cur = String::from("Euro"),
+        _ => out_cur = String::from("N/A"),
     }
     return out_cur;
 }
+
 fn main() {
     let cli_args = CliArguments::parse();
-    println!("{} {} is x {}", &cli_args.amount, &cli_args.first_currency, &cli_args.second_currency);
+    println!("{} {} is x {}", &cli_args.amount, match_currency_name(&cli_args.first_currency), match_currency_name(&cli_args.second_currency));
 }
